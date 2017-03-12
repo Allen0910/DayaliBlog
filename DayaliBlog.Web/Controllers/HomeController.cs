fb269bb3e22f2d1024960be3929511d40198d017
Allen0910 <cuiruiming@hotmail.com>
2017-03-12 16:39:35 +0800

    修复迁移中产生的bug

--- DayaliBlog.Web/Controllers/HomeController.cs
+++ DayaliBlog.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DayaliBlog_Web.Controllers
+namespace DayaliBlog.Web.Controllers
 {
     public class HomeController : Controller
     {
--- DayaliBlog.Web/Startup.cs
+++ DayaliBlog.Web/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace DayaliBlog_Web
+namespace DayaliBlog.Web
 {
     public class Startup
     {

b74bf7486e1ca5f02a6f8b20a61775f036545d4e
Allen <cuiruiming@hotmail.com>
2017-05-25 23:33:22 +0800

    上传博客前端

--- DayaliBlog.Web/Controllers/HomeController.cs
+++ DayaliBlog.Web/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace DayaliBlog.Web.Controllers
             return View();
         }
 
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
